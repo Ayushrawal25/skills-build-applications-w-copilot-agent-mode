eeee0c4c334e224a8dab811152ecb3e9a271927c
Ayush Rawal <114392934+Ayushrawal25@users.noreply.github.com>
2026-02-04 13:05:24 +0000

    Refactor: use Django ORM in populate_db.py for test data creation

--- octofit-tracker/backend/octofit_tracker/core/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/core/management/commands/populate_db.py
@@ -1,50 +1,53 @@
+
 from django.core.management.base import BaseCommand
-from pymongo import MongoClient
+from octofit_tracker.core.models import User, Team, Activity, Leaderboard, Workout
+from django.db import transaction
 
 class Command(BaseCommand):
     help = 'Populate the octofit_db database with test data'
 
     def handle(self, *args, **options):
-        # Connect to MongoDB directly for index creation
-        client = MongoClient('mongodb://localhost:27017')
-        db = client['octofit_db']
-        db.users.drop()
-        db.teams.drop()
-        db.activities.drop()
-        db.leaderboard.drop()
-        db.workouts.drop()
-        db.users.create_index([("email", 1)], unique=True)
+        with transaction.atomic():
+            User.objects.all().delete()
+            Team.objects.all().delete()
+            Activity.objects.all().delete()
+            Leaderboard.objects.all().delete()
+            Workout.objects.all().delete()
 
-        # Sample data
-        users = [
-            {"name": "Superman", "email": "superman@dc.com", "team": "dc"},
-            {"name": "Batman", "email": "batman@dc.com", "team": "dc"},
-            {"name": "Wonder Woman", "email": "wonderwoman@dc.com", "team": "dc"},
-            {"name": "Iron Man", "email": "ironman@marvel.com", "team": "marvel"},
-            {"name": "Captain America", "email": "cap@marvel.com", "team": "marvel"},
-            {"name": "Black Widow", "email": "widow@marvel.com", "team": "marvel"},
-        ]
-        teams = [
-            {"name": "marvel", "members": ["ironman@marvel.com", "cap@marvel.com", "widow@marvel.com"]},
-            {"name": "dc", "members": ["superman@dc.com", "batman@dc.com", "wonderwoman@dc.com"]},
-        ]
-        activities = [
-            {"user": "superman@dc.com", "activity": "flying", "duration": 60},
-            {"user": "ironman@marvel.com", "activity": "flying", "duration": 45},
-            {"user": "batman@dc.com", "activity": "training", "duration": 30},
-            {"user": "cap@marvel.com", "activity": "running", "duration": 50},
-        ]
-        leaderboard = [
-            {"team": "marvel", "points": 120},
-            {"team": "dc", "points": 110},
-        ]
-        workouts = [
-            {"name": "Hero HIIT", "suggested_for": "marvel"},
-            {"name": "Justice Jog", "suggested_for": "dc"},
-        ]
-        db.users.insert_many(users)
-        db.teams.insert_many(teams)
-        db.activities.insert_many(activities)
-        db.leaderboard.insert_many(leaderboard)
-        db.workouts.insert_many(workouts)
-        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data.'))
+            users = [
+                User(name="Superman", email="superman@dc.com", team="dc"),
+                User(name="Batman", email="batman@dc.com", team="dc"),
+                User(name="Wonder Woman", email="wonderwoman@dc.com", team="dc"),
+                User(name="Iron Man", email="ironman@marvel.com", team="marvel"),
+                User(name="Captain America", email="cap@marvel.com", team="marvel"),
+                User(name="Black Widow", email="widow@marvel.com", team="marvel"),
+            ]
+            User.objects.bulk_create(users)
+
+            teams = [
+                Team(name="marvel", members=["ironman@marvel.com", "cap@marvel.com", "widow@marvel.com"]),
+                Team(name="dc", members=["superman@dc.com", "batman@dc.com", "wonderwoman@dc.com"]),
+            ]
+            Team.objects.bulk_create(teams)
+
+            activities = [
+                Activity(user="superman@dc.com", activity="flying", duration=60),
+                Activity(user="ironman@marvel.com", activity="flying", duration=45),
+                Activity(user="batman@dc.com", activity="training", duration=30),
+                Activity(user="cap@marvel.com", activity="running", duration=50),
+            ]
+            Activity.objects.bulk_create(activities)
+
+            leaderboard = [
+                Leaderboard(team="marvel", points=120),
+                Leaderboard(team="dc", points=110),
+            ]
+            Leaderboard.objects.bulk_create(leaderboard)
+
+            workouts = [
+                Workout(name="Hero HIIT", suggested_for="marvel"),
+                Workout(name="Justice Jog", suggested_for="dc"),
+            ]
+            Workout.objects.bulk_create(workouts)
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data using Django ORM.'))
