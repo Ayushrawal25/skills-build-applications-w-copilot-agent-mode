0b1c7a3ddb84e3a82bd2160443a625a811ef8f7d
Ayush Rawal <114392934+Ayushrawal25@users.noreply.github.com>
2026-02-05 04:56:47 +0000

    Fix: Use Django ORM in populate_db.py for test data creation

--- octofit-tracker/backend/octofit_tracker/core/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/core/management/commands/populate_db.py
@@ -8,46 +8,42 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         with transaction.atomic():
+            self.stdout.write(self.style.SUCCESS('Clearing old data...'))
             User.objects.all().delete()
             Team.objects.all().delete()
             Activity.objects.all().delete()
             Leaderboard.objects.all().delete()
             Workout.objects.all().delete()
 
-            users = [
-                User(name="Superman", email="superman@dc.com", team="dc"),
-                User(name="Batman", email="batman@dc.com", team="dc"),
-                User(name="Wonder Woman", email="wonderwoman@dc.com", team="dc"),
-                User(name="Iron Man", email="ironman@marvel.com", team="marvel"),
-                User(name="Captain America", email="cap@marvel.com", team="marvel"),
-                User(name="Black Widow", email="widow@marvel.com", team="marvel"),
-            ]
-            User.objects.bulk_create(users)
+            self.stdout.write(self.style.SUCCESS('Creating teams...'))
+            marvel = Team.objects.create(name='Team Marvel', members=[])
+            dc = Team.objects.create(name='Team DC', members=[])
 
-            teams = [
-                Team(name="marvel", members=["ironman@marvel.com", "cap@marvel.com", "widow@marvel.com"]),
-                Team(name="dc", members=["superman@dc.com", "batman@dc.com", "wonderwoman@dc.com"]),
-            ]
-            Team.objects.bulk_create(teams)
+            self.stdout.write(self.style.SUCCESS('Creating users...'))
+            spiderman = User.objects.create(name='Spider-Man', email='spiderman@marvel.com', team=marvel.name)
+            ironman = User.objects.create(name='Iron Man', email='ironman@marvel.com', team=marvel.name)
+            wonderwoman = User.objects.create(name='Wonder Woman', email='wonderwoman@dc.com', team=dc.name)
+            batman = User.objects.create(name='Batman', email='batman@dc.com', team=dc.name)
 
-            activities = [
-                Activity(user="superman@dc.com", activity="flying", duration=60),
-                Activity(user="ironman@marvel.com", activity="flying", duration=45),
-                Activity(user="batman@dc.com", activity="training", duration=30),
-                Activity(user="cap@marvel.com", activity="running", duration=50),
-            ]
-            Activity.objects.bulk_create(activities)
+            marvel.members = [spiderman.email, ironman.email]
+            dc.members = [wonderwoman.email, batman.email]
+            marvel.save()
+            dc.save()
 
-            leaderboard = [
-                Leaderboard(team="marvel", points=120),
-                Leaderboard(team="dc", points=110),
-            ]
-            Leaderboard.objects.bulk_create(leaderboard)
+            self.stdout.write(self.style.SUCCESS('Creating activities...'))
+            Activity.objects.create(user=spiderman.email, activity='Running', duration=30)
+            Activity.objects.create(user=ironman.email, activity='Cycling', duration=45)
+            Activity.objects.create(user=wonderwoman.email, activity='Swimming', duration=60)
+            Activity.objects.create(user=batman.email, activity='Yoga', duration=40)
 
-            workouts = [
-                Workout(name="Hero HIIT", suggested_for="marvel"),
-                Workout(name="Justice Jog", suggested_for="dc"),
-            ]
-            Workout.objects.bulk_create(workouts)
+            self.stdout.write(self.style.SUCCESS('Creating leaderboard...'))
+            Leaderboard.objects.create(team=marvel.name, points=150)
+            Leaderboard.objects.create(team=dc.name, points=130)
 
-        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data using Django ORM.'))
+            self.stdout.write(self.style.SUCCESS('Creating workouts...'))
+            Workout.objects.create(name='Push-ups', suggested_for=spiderman.team)
+            Workout.objects.create(name='Sit-ups', suggested_for=ironman.team)
+            Workout.objects.create(name='Squats', suggested_for=wonderwoman.team)
+            Workout.objects.create(name='Plank', suggested_for=batman.team)
+
+            self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data!'))
