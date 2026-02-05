d7f47900a80b9b29bc515501adf0d4d35d3d5f2e
Ayush Rawal <114392934+Ayushrawal25@users.noreply.github.com>
2026-02-05 04:46:09 +0000

    Implement Octofit Tracker REST API endpoints, admin, and tests for all collections

--- octofit-tracker/backend/octofit_tracker/core/tests.py
+++ octofit-tracker/backend/octofit_tracker/core/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from .models import User, Team, Activity, Leaderboard, Workout
+
+class UserApiTests(APITestCase):
+	def test_list_users(self):
+		url = reverse('user-list')
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 200)
+
+class TeamApiTests(APITestCase):
+	def test_list_teams(self):
+		url = reverse('team-list')
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 200)
+
+class ActivityApiTests(APITestCase):
+	def test_list_activities(self):
+		url = reverse('activity-list')
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 200)
+
+class LeaderboardApiTests(APITestCase):
+	def test_list_leaderboard(self):
+		url = reverse('leaderboard-list')
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 200)
+
+class WorkoutApiTests(APITestCase):
+	def test_list_workouts(self):
+		url = reverse('workout-list')
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 200)
--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -13,9 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': request.build_absolute_uri('users/'),
+        'teams': request.build_absolute_uri('teams/'),
+        'activities': request.build_absolute_uri('activities/'),
+        'leaderboard': request.build_absolute_uri('leaderboard/'),
+        'workouts': request.build_absolute_uri('workouts/'),
+    })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', api_root, name='api-root'),
+    path('', include('octofit_tracker.core.urls')),
 ]
